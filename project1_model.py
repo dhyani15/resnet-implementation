e3ffae36b748a2392598ef9a41c1720edfddd8ac
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-03-22 04:48:18 -0400

    Experiment 12.

--- project1_model.py
+++ project1_model.py
@@ -35,16 +35,16 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         # c1
-        self.in_planes = 64
+        self.in_planes = 32
         self.num_blocks = num_blocks
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=5,
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5,
                                stride=1, padding=2, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
         # self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         # self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(128, num_classes)
+        self.linear = nn.Linear(64, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -60,11 +60,11 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         # out = self.layer3(out)
         # out = self.layer4(out)
-        out = F.avg_pool2d(out, 16)
+        out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
 def project1_model():
-    return ResNet(BasicBlock, [4, 4])
+    return ResNet(BasicBlock, [2, 2, 2, 2])
