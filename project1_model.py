b11c61766878e28a6d9377d898ff9dfdf9e25e31
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-03-23 05:35:52 -0400

    Vanilla Resnet.

--- project1_model.py
+++ project1_model.py
@@ -19,7 +19,7 @@ class BasicBlock(nn.Module):
         if stride != 1 or in_planes != planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, planes,
-                          kernel_size=5, stride=stride, padding=2, bias=False),
+                          kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes)
             )
 
@@ -35,17 +35,17 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         # c1
-        self.in_planes = 32
+        self.in_planes = 64
         self.num_blocks = num_blocks
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3,
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=2)
-        self.dropout = nn.Dropout(0.25)
-        self.linear = nn.Linear(256, num_classes)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        # self.dropout = nn.Dropout(0.25)
+        self.linear = nn.Linear(512, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -58,11 +58,11 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
-        out = self.dropout(out)
+        # out = self.dropout(out)
         out = self.layer2(out)
-        out = self.dropout(out)
+        # out = self.dropout(out)
         out = self.layer3(out)
-        out = self.dropout(out)
+        # out = self.dropout(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
