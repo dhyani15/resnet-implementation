846313be81c5a3ff3430b1ebcafcced4ff5a13a3
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-03-22 23:52:21 -0400

    Testing.

--- test2.py
+++ test2.py
@@ -1,168 +1,70 @@
 import torch
-import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+from project1_model import project1_model
+from utils import progress_bar
 
 
-# Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = project1_model().to(device)
+model_path = './project1_model.pt'
+checkpoint = torch.load(model_path, map_location=device)
+model.load_state_dict(checkpoint['net'], strict=False)
 
-# Hyper-parameters
-num_epochs = 150
-learning_rate = 0.1
+best_acc = checkpoint['acc']
+print(best_acc)
 
-# Image preprocessing modules
-transform = transforms.Compose([
-    transforms.Pad(4),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(32),
-    transforms.ToTensor()])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
-# CIFAR-10 dataset
-train_dataset = torchvision.datasets.CIFAR10(root='./data1',
-                                             train=True, 
-                                             transform=transform,
-                                             download=True)
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
 
-test_dataset = torchvision.datasets.CIFAR10(root='./data1',
-                                            train=False, 
-                                            transform=transforms.ToTensor())
-
-# Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=100, 
-                                           shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=100, 
-                                          shuffle=False)
-
-# 3x3 convolution
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3, 
-                     stride=stride, padding=1, bias=False)
-
-# Residual block
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
-
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
-        return out
-
-# ResNet
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_channels = 64
-        self.conv = conv3x3(3, 64)
-        self.bn = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self.make_layer(block, 64, layers[0])
-        self.layer2 = self.make_layer(block, 128, layers[1], 2)
-        # self.layer3 = self.make_layer(block, 64, layers[2], 2)
-        self.avg_pool = nn.AvgPool2d(16)
-        self.fc = nn.Linear(128, num_classes)
-
-    def make_layer(self, block, out_channels, blocks, stride=1):
-        downsample = None
-        if (stride != 1) or (self.in_channels != out_channels):
-            downsample = nn.Sequential(
-                conv3x3(self.in_channels, out_channels, stride=stride),
-                nn.BatchNorm2d(out_channels))
-        layers = []
-        layers.append(block(self.in_channels, out_channels, stride, downsample))
-        self.in_channels = out_channels
-        for i in range(1, blocks):
-            layers.append(block(out_channels, out_channels))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = self.conv(x)
-        out = self.bn(out)
-        out = self.relu(out)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        # out = self.layer3(out)
-        out = self.avg_pool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-model = ResNet(ResidualBlock, [2, 2]).to(device)
-# resnet_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-# print("Number of trainable parameters in the model: %d\n"%(resnet_total_params))
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-    # torch.numel() returns number of elements in a tensor
-
-print(count_parameters(model))
-
-
-# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate, weight_decay=5e-4)
 
-# For updating learning rate
-def update_lr(optimizer, lr):    
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+test_loss_history = []
 
-# Train the model
-total_step = len(train_loader)
-curr_lr = learning_rate
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
 
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if (i+1) % 100 == 0:
-            print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}".format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-
-    # Decay learning rate
-    if (epoch+1) % 20 == 0:
-        curr_lr /= 3
-        update_lr(optimizer, curr_lr)
-
-# Test the model
-model.eval()
-with torch.no_grad():
+def test(epoch):
+    global best_acc
+    model.eval()
+    test_loss = 0
     correct = 0
     total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
 
-    print('Accuracy of the model on the test images: {} %'.format(100 * correct / total))
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            if batch_idx == len(testloader)-1:
+                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        test_loss = test_loss/len(testloader)
+        test_loss_history.append(test_loss)
 
-# Save the model checkpoint
-torch.save(model.state_dict(), 'resnet.ckpt')
+
+test(5)
+
+# model.eval()
+# corrects = 0
+# for batch_idx, (inputs, labels) in enumerate(testloader, 1):
+#     with torch.set_grad_enabled(False):
+#         outputs = model(inputs)
+#         _, preds = torch.max(outputs, 1)
+#     corrects += torch.sum(preds == labels.data)
+# print(corrects.float() / len(testloader))
+
+
+# pred = predicted_output.data.max(1)[1] # get the index of the max log-probability
+#       test_corrects += pred.eq(labels.data).cpu().sum()
+#       test_accuracy = 100. * test_corrects / len(testDataLoader.dataset)
