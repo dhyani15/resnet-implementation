d2e59baff09dcb7ab5c324adce351ad7d99e5e47
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-03-24 00:30:54 -0400

    Exp 38.

--- project1_model.py
+++ project1_model.py
@@ -9,17 +9,17 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=5, stride=stride, padding=2, bias=False)
+            in_planes, planes, kernel_size=7, stride=stride, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=5,
-                               stride=1, padding=2, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=7,
+                               stride=1, padding=3, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, planes,
-                          kernel_size=3, stride=stride, padding=1, bias=False),
+                          kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes)
             )
 
@@ -37,8 +37,8 @@ class ResNet(nn.Module):
         # c1
         self.in_planes = 32
         self.num_blocks = num_blocks
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=5,
-                               stride=1, padding=2, bias=False)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=7,
+                               stride=1, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
@@ -71,7 +71,7 @@ class ResNet(nn.Module):
 
 
 def project1_model():
-    return ResNet(BasicBlock, [4, 4])
+    return ResNet(BasicBlock, [2, 2])
 
 
 net = project1_model()
