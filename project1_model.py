55181b9d237b93fce9858a42c91c1ac2d577ee11
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-03-24 00:44:19 -0400

    Exp 39.

--- project1_model.py
+++ project1_model.py
@@ -42,10 +42,10 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
         self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
-        # self.layer3 = self._make_layer(block, 132, num_blocks[2], stride=2)
+        self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
         # self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         # self.dropout = nn.Dropout(0.25)
-        self.linear = nn.Linear(64, num_classes)
+        self.linear = nn.Linear(128, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -61,17 +61,17 @@ class ResNet(nn.Module):
         # out = self.dropout(out)
         out = self.layer2(out)
         # out = self.dropout(out)
-        # out = self.layer3(out)
+        out = self.layer3(out)
         # out = self.dropout(out)
         # out = self.layer4(out)
-        out = F.avg_pool2d(out, 16)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
 def project1_model():
-    return ResNet(BasicBlock, [2, 2])
+    return ResNet(BasicBlock, [2, 2, 2])
 
 
 net = project1_model()
